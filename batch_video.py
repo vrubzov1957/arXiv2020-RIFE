7b3e4b6648bacdce766c09dec0a002a607e03c24
vrubzov1957 <54937209+vrubzov1957@users.noreply.github.com>
2021-04-06 23:54:00 +0300

    Update batch_video.py

--- batch_video.py
+++ batch_video.py
@@ -2,12 +2,13 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Interpolation for a pair of images')
 parser.add_argument('--list', dest='list', type=str, default=None)
+args = parser.parse_args()
 
-file1 = open('myfile.txt', 'r')
+file1 = open(args.list, 'r')
 Lines = file1.readlines()
  
 count = 0
 # Strips the newline character
 for line in Lines:
-    execfile('inference_video.py --fp16 --exp=2 --video='+line) 
+    execfile('inference_video.py --fp16 --exp=2 --video="'+line+'"') 
  
